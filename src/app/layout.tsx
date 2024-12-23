f51233f8452a5581674c7101829e798966bef9ef
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2024-12-23 10:53:33 +0600

    update layout padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,8 +40,8 @@ export default function RootLayout({
           />
         </head>
         <body
-          className={`${Barlow.variable} ${Almarai.variable} antialiased max-w-[1440px] mx-auto px-5
-          md:px-10 bg-neutrals-black text-neutrals-white relative scroll-smooth overflow-x-hidden`}
+          className={`${Barlow.variable} ${Almarai.variable} antialiased max-w-[1440px] mx-auto px-1
+          md:px-8 bg-neutrals-black text-neutrals-white relative scroll-smooth overflow-x-hidden`}
         >
           <AntdRegistry>
             <NavBar />
